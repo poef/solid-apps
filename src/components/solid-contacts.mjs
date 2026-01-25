563b264a7cf2ee7d906d89719011118440fd0cf6
Auke van Slooten <auke@muze.nl>
2026-01-25 15:56:52 +0100

    improved letter navigation

--- src/components/solid-contacts.mjs
+++ src/components/solid-contacts.mjs
@@ -9,39 +9,68 @@ export default {
 <div class="solid-contacts" data-flow-map="contacts.view">
 	<template>
 		<div class="solid-contacts-section">
-			<div class="solid-contacts-letter" data-flow-field="capital"></div>
+			<a class="solid-contacts-letter" data-flow-field="capital" data-simply-command="solid-contacts-letter-nav"></a>
 			<div class="solid-contacts-list" data-flow-list="entries">
 				<template>
-					<div class="solid-contacts-entry">
+					<a class="solid-contacts-entry" data-simply-command="solid-contacts-show">
 						<span class="solid-contacts-avatar"></span>
 						<span class="solid-contacts-lastname" data-flow-field="last_name"></span>,
 						<span class="solid-contacts-firstname" data-flow-field="first_name"></span>
-					</div>
+					</a>
 				</template>
 			</div>
 		</div>
 	</template>
-</div>`
+</div>`,
+		'solid-contacts-filter':html`
+<div class="solid-contacts-filter">
+	<input type="search" class="solid-contacts-filter-input"
+		data-simply-command="solid-contacts-filter" 
+		data-simply-immediate
+		data-simply-value="contacts">
+</div>
+		`
 	},
 	css: {
+		'solid': css`
+			@layer theme {
+				:root {
+					--ds-box-radius: 0;
+					scroll-behavior: smooth;
+				}
+				.solid {
+					width: 100%;
+					height: 100%;
+					min-height: 100vh;
+					min-width: 100vw;
+				}
+			}
+		`,
 		'solid-contacts': css`
 			@layer component {
 				.solid-sticky-top {
 					position: sticky;
 					top: 0;
 					z-index: 10;
-					background: var(--ds-white);
+					background: var(--ds-color-background);
+				}
+				.solid-contacts {
+					display: flow-root;
 				}
 				.solid-contacts-letter {
+					position: sticky;
+					z-index: 9;
+					top: calc(2 * var(--ds-line-height));
+					text-decoration: none;
+					scroll-margin-top: calc(2 * var(--ds-line-height));
+					display: block;
+					margin: var(--ds-space-d4) var(--ds-space-d4) 0 0;
 					background: var(--ds-primary);
 					color: var(--ds-primary-contrast);
 					width: calc(var(--ds-space-d2) + var(--ds-line-height));
 					padding: var(--ds-space-d4);
 					text-align: center;
-					margin: var(--ds-space-d4) var(--ds-space-d2) 0 0;
-					position: sticky;
-					z-index: 9;
-					top: calc(2 * var(--ds-line-height));
+
 				}
 				.solid-contacts-entry {
 					display: flex;
@@ -51,9 +80,40 @@ export default {
 					width: calc(var(--ds-space-d2) + var(--ds-line-height));
 					aspect-ratio: 1;
 					margin: var(--ds-space-d4) var(--ds-space-d2) 0 0;
-					background-color: var(--ds-grey-80);
+					background-color: var(--ds-grey-medium);
+				}
+				.solid-contacts-letters-nav .solid-contacts-section {
+					float: left;
+				}
+				.solid-contacts-letters-nav .solid-contacts-entry {
+					display: none;
+				}
+				.solid-contacts-letters-nav .solid-contacts-letter {
+					float: left;
+				}
+				.solid-contacts-filter {
+					position: sticky;
+					bottom: 0;
+					z-index: 10;
+					background: var(--ds-white);
 				}
 			}
 		`
+	},
+	commands: {
+		'solid-contacts-letter-nav': async function(el, value) {
+			const contacts = el.closest('.solid-contacts')
+			contacts.classList.toggle('solid-contacts-letters-nav')
+			if (!contacts.classList.contains('solid-contacts-letters-nav')) {
+				window.location.hash = el.hash
+				window.setTimeout(() => {
+					el.scrollIntoView({behavior:'smooth'})
+				}, 100)
+			}
+		},
+		'solid-contacts-filter': async function(el, value) {
+			const model = simply.path.get(this.state, el.dataset.simplyValue)
+			model.state.options.name.filters.text = el.value
+		}
 	}
 }
